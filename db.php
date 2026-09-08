74946a55a6c3a24b221d551d83477e6f9e9a19a8
FatJoeGames <52460183+FatJoeGames@users.noreply.github.com>
2026-09-08 23:44:32 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
 // db.php
-$dsn = "mysql:host=127.0.0.1;dbname=zoneup_sec;charset=utf8mb4";
+$dsn = "mysql:host=127.0.0.1;dbname=ht_db_sec;charset=utf8mb4";
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES => false,
 ];
 try {
-    $pdo = new PDO($dsn, 'zoneup_user', 'ZoneUpSecure2026!', $options);
+    $pdo = new PDO($dsn, 'ht_db_usr', 'ht_db_sec2026!', $options);
 } catch (\PDOException $e) {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
